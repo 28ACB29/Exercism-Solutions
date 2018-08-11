5ae6dcff2c6b62d928ed71a21ded1f688d6846e5
Arthur Baroi <arthurcbaroi@hotmail.com>
2018-08-11 09:24:13 -0500

    Adding current solutions
    Pushing what I have so far.

---
+++ fsharp/bob/Bob.fs
@@ -0,0 +1,10 @@
+﻿module Bob
+
+let response (input: string): string =
+    let trimmed: string = input.Trim()
+    match trimmed with
+    | trimmed when trimmed.EndsWith("?") -> "Sure."
+    | trimmed when trimmed.EndsWith("!") -> "Whoa, chill out!"
+    | trimmed when trimmed.EndsWith("?!") -> "Calm down, I know what I'm doing!"
+    | "" -> "Fine. Be that way!"
+    | _ -> "Whatever!"
---
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,15 @@
+﻿module SumOfMultiples
+
+let sum (numbers: int list) (upperBound: int): int =
+    let generateMultiples (n: int): int list =
+        let inclusiveNumber: int = upperBound / n
+        let boundary: int =
+            match upperBound % n = 0 with
+            | true -> 1
+            | false -> 0
+        let exclusiveNumber: int = inclusiveNumber - boundary
+        List.init exclusiveNumber (fun i -> (i + 1) * n)
+    number
+    |> List.collect generateMultiples
+    |> List.distinct
+    |> List.sum
